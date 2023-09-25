ea830fd044173625d3932414089eb0573e5acfa9
Dipu Haider <dipuhaider@gmail.com>
2023-09-25 11:49:28 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "donation-primary": "#FF444A",
+        "donation-dark": "#0B0B0B",
+        "donation-txt40": "#0B0B0B66",
+        "donation-txt50": "#0B0B0B80",
+        "donation-txt70": "#0B0B0BB2",
+        "btn-green": "#009444",
+        "status-green": "#00C49F",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
