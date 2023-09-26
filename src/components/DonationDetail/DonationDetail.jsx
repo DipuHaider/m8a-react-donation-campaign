2108220b9c52f0c1c5994049d4dba7cc89654131
Dipu Haider <dipuhaider@gmail.com>
2023-09-26 19:39:12 +0600

    Design Donation Details Page

--- src/components/DonationDetail/DonationDetail.jsx
+++ src/components/DonationDetail/DonationDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import './DonationDetail.css';
 
 const DonationDetail = () => {
 
@@ -19,11 +20,20 @@ const DonationDetail = () => {
     // }
 
     return (
-        <div>
+        
+        <div className="donationDetail-container relative items-center justify-center">
             {/* <button onClick={handleGoBack}>Go back</button> */}
-            <div className="grid grid-cols-4">
-                    {donation.title}
+            <div className="flex flex-col mt-32 relative">
+                <img className="rounded-2xl w-full" src={donation.picture} alt={donation.title} />
+                <div className="bg-donation-txt50 min-h-[130px] p-9 rounded-b-2xl absolute left-0 right-0 bottom-0">
+                <button className="bg-donation-primary rounded btn text-white">Donate {donation.price}</button>
+                </div>
+                
             </div>
+            <div>
+                    <h1>{donation.title}</h1>
+                    <p>{donation.description}</p>
+                </div>
         </div>
     );
 };
