ee806bef3a0ef4ddd4814a25df7d7c00f210e95f
Dipu Haider <dipuhaider@gmail.com>
2023-09-26 16:08:44 +0600

    Update Banner Section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,24 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="header-container">
-            <div className="navbar">
-                <div className="flex-1">
-                    <img src="../../../public/Logo.png" alt="" />
-                </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal px-1">
-                        <nav>
-                            <NavLink to="/">Home</NavLink>
-                            <NavLink to="/donations">Donation</NavLink>
-                            <NavLink to="/statistics">Statistics</NavLink>
-                        </nav>
-                    </ul>
+        <div className="absolute inset-x-0 top-0 z-50">
+            <div className="container mx-auto max-w-7xl">
+                <div className="header-container">
+                    {/* Header content */}
+                    <div className="navbar">
+                        <div className="flex-1">
+                            <img src="../../../public/Logo.png" alt="" />
+                        </div>
+                        <div className="flex-none">
+                            <ul className="menu menu-horizontal px-1">
+                                <nav>
+                                    <NavLink to="/">Home</NavLink>
+                                    <NavLink to="/donations">Donation</NavLink>
+                                    <NavLink to="/statistics">Statistics</NavLink>
+                                </nav>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
