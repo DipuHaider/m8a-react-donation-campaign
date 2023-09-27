ce290fc914f55c87b796faf406688e44682c38e3
Dipu Haider <dipuhaider@gmail.com>
2023-09-27 09:34:45 +0600

    Update DonationDetails.jsx

--- src/components/DonationDetail/DonationDetail.jsx
+++ src/components/DonationDetail/DonationDetail.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useParams, useNavigate } from "react-router-dom";
 import './DonationDetail.css';
 
 const DonationDetail = () => {
@@ -7,33 +7,33 @@ const DonationDetail = () => {
     const {id} = useParams();
     const idInt = parseInt(id);
     const donation = donations.find(donation => donation.id === idInt);
-    // console.log(id);
-    console.log(donation);
-    
-    // const navigate = useNavigate();
 
-    // const{id, title, price} = donation;
-    // console.log({id})
+    const customButtonColor = {
+        backgroundColor: donation.text_button_bg,
+     }
 
-    // const handleGoBack = () => {
-    //     navigate(-1);
-    // }
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    }
 
     return (
         
-        <div className="donationDetail-container relative items-center justify-center">
-            {/* <button onClick={handleGoBack}>Go back</button> */}
+        <div className="donationDetail-container relative items-center justify-center space-y-7">
+            
             <div className="flex flex-col mt-32 relative">
                 <img className="rounded-2xl w-full" src={donation.picture} alt={donation.title} />
                 <div className="bg-donation-txt50 min-h-[130px] p-9 rounded-b-2xl absolute left-0 right-0 bottom-0">
-                <button className="bg-donation-primary rounded btn text-white">Donate {donation.price}</button>
+                <button style={customButtonColor} className="rounded btn text-white border-none">Donate {donation.price}</button>
                 </div>
-                
+            </div>
+            <div className="space-y-4">
+                <h1 className="text-4xl font-bold text-donation-dark">{donation.title}</h1>
+                <p className="text-donation-txt70 text-base font-normal leading-7 text-justify">{donation.description}</p>
             </div>
             <div>
-                    <h1>{donation.title}</h1>
-                    <p>{donation.description}</p>
-                </div>
+                <button onClick={handleGoBack}>Go back</button>
+            </div>
         </div>
     );
 };
